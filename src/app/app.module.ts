21452c016eb3e7daeb02adc91a10d311b2bce125
khurram <kkh@facelift-bbt.com>
2022-09-30 19:43:05 +0500

    updates: delete folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DetailPageComponent } from './detail-page/detail-page.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { WorkInProgressComponent } from './work-in-progress/work-in-progress.component';
@@ -12,17 +11,12 @@ import { WorkInProgressComponent } from './work-in-progress/work-in-progress.com
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent, 
+    HeaderComponent,
     NavbarComponent,
     WorkInProgressComponent,
-    DetailPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
